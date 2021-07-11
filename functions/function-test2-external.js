5ad8bb46e2c8574c663534c64c1f4801c0f50559
kenlee <lakoo.kenlee@gmail.com>
2021-07-11 18:29:36 +0800

    Adding more testing

--- functions/function-test2-external.js
+++ functions/function-test2-external.js
@@ -1,3 +1,10 @@
+aDummyAlert();
+console.log("external js is executed");
+
 function externalFunc() {
     return "from external src";
+}
+
+function aDummyAlert() {
+    alert("external.js is ready");
 }
--- note.md
+++ note.md
@@ -1,1 +1,5 @@
 https://github.com/getify/You-Dont-Know-JS
+
+
+first edition
+https://github.com/getify/You-Dont-Know-JS/blob/1st-ed/README.md
